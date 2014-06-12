51d5e4345303947dc75f7d3cf6effd8c319779eb
sicho <sicho.net@gmail.com>
2014-06-13 03:56:37 +0900

    gunicorn対応

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
-sys.path.append('vendor/bottle')
 
-from bottle import route, static_file, run
+from bottle import route, static_file, default_app
 from app.controllers import *
 
 @route('/public/<filepath:path>')
 def server_static(filepath):
     return static_file(filepath, root='./public')
 
-run(host='192.168.58.11', port=8080, debug=True, reloader=True)
+app = default_app()
